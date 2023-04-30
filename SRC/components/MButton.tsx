3b794916370148a72915dd5684c3a2c2ed4c800c
mahdi <mahdi@gmail.com>
2023-04-30 22:27:28 +0430

    initial Bare Project

---
+++ SRC/components/MButton.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+interface props {}
+
+export default function MButton() {
+  return (
+    <View>
+      <Text>MButton</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
